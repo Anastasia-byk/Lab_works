ec3c6932b56e4955fb364a4be858b067b4c2fa05
Anastasia-byk <anastasiabyk04@gmail.com>
2023-03-25 16:24:42 +0300

    Lab 3 queeues final

--- laba_3_queues/laba_3_queues/Source.cpp
+++ laba_3_queues/laba_3_queues/Source.cpp
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
@@ -8,6 +9,7 @@
 #define FROM_END 1
 using namespace std;
 
+
 struct List 
 {
 	int info;
@@ -15,152 +17,273 @@ struct List
 };
 
 
-List* AddToList(List*, List*, int, int);
+List* createList(int info);
+void AddToList(List**, int, int);
+void deleteAtBeginning(List**);
 void ViewList(List*, List*, int);
-void DellElement(List*, List*);
-void MemoryCleaning(List*, List*);
-void RemoveNegativeElements(List*, List*);
+void MemoryCleaning(List*);
+void RemoveNegativeElements(List**);
 
 
 
 int main() 
 {
-	int choice, amountOfElements, element;
+	int choice, amountOfElements, element, addChoice, beginOrEndChoice;
 	List* head = new List;
 	head->prev = head->next = NULL;
 	List* tail = head;
+	int info;
+	int isCreatingList = 0;
 
 
 	while (1) {
-		printf("\n 1 - Create  2 - Add to start 3 - Add to the end 4 - View from the beginning 5 - View from the end 6 - Remove negative elements 7 - Clear memory  0 - Exit DICK\n");
+		printf("\n 1 - Create  2 - Add element 3 - View list 4 - Remove negative elements 5 - Clear memory Else - Exit\n");
 		scanf_s("%d", &choice);
 		switch (choice) 
 		{
-		case 1: if (head->next != NULL) {
+		case 1: if (head->next != NULL) {					
 					printf("Clear memory!");
 					break;
 				}
+			  isCreatingList = 1;
 			  printf("List was created!");
 			  break;
-		case 2: case 3: printf("Enter amount of elements: ");
-						while (!(cin >> amountOfElements))
-						{
-							printf("---error---");
-							return 0;
+		case 2:												
+			if (isCreatingList == 0)
+			{
+				printf("List is not created");
+				break;
+			}
+			printf("Enter amount of elements: ");			
+			while (!(cin >> amountOfElements))
+			{
+				printf("---error---");
+				return 0;
 
-						}
-						for (int i = 1; i <= amountOfElements; i++)
-						{
-							element = rand() % 100 - 50;
-							if (choice == 2)
-								head = AddToList(head, tail, element, FROM_START);
-							else
-								tail = AddToList(head, tail, element, FROM_END);
-
-						}
-						printf("Elements were added!");
+			}
+			printf("How to complete the list: 1 - random, 2 - from the keyboard\n ");
+			while (!(cin >> addChoice)) 
+			{
+				printf("---error---");
+				return 0;
+			}
+			if (addChoice == 1) 
+			{
+				printf("Where to add elements: 1 - Beginning 2 - End\n");
+				while (!(cin >> beginOrEndChoice)) 
+				{
+					printf("---error---");
+					return 0;
+				}
+				for (int i = 1; i <= amountOfElements; i++)
+				{
+					info = rand() % 100 - 50;
+					if (isCreatingList)
+					{
+						head = createList(info);
+						isCreatingList = 0;
+						continue;
+					}
+					if (beginOrEndChoice == 1) 
+					{
+						AddToList(&head, info, FROM_START);
+					}
+					else if (beginOrEndChoice == 2)
+					{
+						AddToList(&head, info, FROM_END);
+					}
+					else
+						return 0;
+				}
+				printf("Elements were added!");
+			}
+			else if (addChoice == 2) 
+			{
+				printf("Where to add elements: 1 - Beginning 2 - End\n");
+				while (!(cin >> beginOrEndChoice))
+				{
+					printf("---error---");
+					return 0;
+				}
+				for (int i = 0; i < amountOfElements; i++) 
+				{
+					printf("Enter element %d: ", i + 1);
+					while (!(cin >> info)) 
+					{
+						printf("---error---");
+						return 0;
+					}
+					if (isCreatingList)
+					{
+						head = createList(info);
+						isCreatingList = 0;
+						continue;
+					}
+					if (beginOrEndChoice == 1)
+					{
+						AddToList(&head, info, FROM_START);
+					}
+					else if (beginOrEndChoice == 2)
+					{
+						AddToList(&head, info, FROM_END);
+					}
+					else
+						return 0;
+				}
+			}
 						break;
+		case 3:
+			printf("How to view the list: 1 - From beginning 2 - From end\n");
+			while (!(cin >> beginOrEndChoice))
+			{
+				printf("---error---");
+				return 0;
+			}
+			if (beginOrEndChoice == 1)
+			{
+				ViewList(head, tail, FROM_START);
+			}
+			else if (beginOrEndChoice == 2)
+			{
+				tail = head;                                                         
+				while (tail->next != NULL)
+					tail = tail->next;
+				ViewList(head, tail, FROM_END);
+			}
+			else
+				return 0;
+			break;
 		case 4:
-			ViewList(head, tail, FROM_START);
+			RemoveNegativeElements(&head);
+			printf("Negative elements were deleted!");
 			break;
 		case 5:
-			ViewList(head, tail, FROM_END);
+			MemoryCleaning(head);			
+			head = new List;
+			head->prev = head->next = NULL;
+			isCreatingList = 0;
+			printf("Memory cleares successfully:)\n");
 			break;
-		case 6:
-			break;
-		case 7:
-			MemoryCleaning(head, tail);
-			printf("Memory cleared!");
-			break;
-
-	
+		default:                                              
+			MemoryCleaning(head);
+			printf("Program is over!");
+			return 0;
 		}
 	}
 
 
 }
 
-List* AddToList(List* head, List* tail, int info, int mode)
+List* createList(int info) 
 {
 	List* temp = new List;
 	temp->info = info;
-	if (head->next == NULL)
-	{
-		temp->prev = head;
-		temp->next = NULL;
-		head->next = temp;
-		tail = temp;
-		if (mode == FROM_START)
+	temp->prev = NULL;
+	temp->next = NULL;
+	return temp;
+}
 
-		return tail;
-	}	
-	
-	if (mode == FROM_START)
+
+void AddToList(List** head, int info, int mode) 
+{
+	List* newList = createList(info);
+	if (*head == NULL)
 	{
-		temp->prev = head;
-		temp->next = head->next;
-		head->next->prev = temp;
-		head->next = temp;
-		return head;
+		*head = newList;
 	}
-	else
+	else if (mode == FROM_START) 
 	{
-		temp->next = NULL;
-		temp->prev = tail;
-		tail->next = temp;
-		tail = temp;
-		return tail;
+		newList->next = *head;
+		(*head)->prev = newList;
+		*head = newList;
+	}
+	else if (mode == FROM_END)
+	{
+		List* temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newList;
+		newList->prev = temp;
+
 	}
 }
 
-void DellElement(List* head, List* tail) 
+
+void deleteAtBeginning(List** head) 
 {
-	List* temp = head->next;
-	if (head->next == NULL) 
+	if (*head == NULL) 
 	{
-		printf("Stack is empty\n");
+		printf("Steck is empty!\n");
 		return;
 	}
-	else if (head->next->next == NULL)
-		head->next = NULL;
-	else
+	List* temp = *head;
+	*head = (*head)->next;
+	if (*head != NULL) 
 	{
-		head->next = head->next->next;
-		head->next->prev = head;
+		(*head)->prev = NULL;
 	}
 	delete temp;
 }
 
-void ViewList(List* head, List* tail, int mode)
+void ViewList(List* head, List* tail, int mode) 
 {
-	List* temp;
-	if (mode == FROM_START)
+	//head = head->next;
+	if (mode == FROM_START) 
 	{
-		temp = head->next;
-		while (temp != NULL)
+		while (head != NULL) 
 		{
-			printf("%d\n", temp->info);
-			temp = temp->next;
+			printf("%d\n", head->info);
+			head = head->next;
 		}
 	}
-	else
+	else if (mode == FROM_END) 
 	{
-		temp = tail;
-		while (temp != head)
+		while (tail != NULL)
 		{
-			printf("%d\n", temp->info);
-			temp = temp->prev;
+			printf("%d\n", tail->info);
+			tail = tail->prev;
 		}
 	}
 }
 
-void MemoryCleaning(List* head, List* tail)
+
+void MemoryCleaning(List* head)       
 {
-	while (head->next != NULL)
-		DellElement(head, tail);
+	List* temp = head;
+	while (temp != NULL) 
+	{
+		List* next = temp->next;
+		delete temp;
+		temp = next;
+	}
 }
 
-void RemoveNegativeElements(List* head, List* tail) 
+void RemoveNegativeElements(List** head)
 {
+	struct List* temp = *head;
+	struct List* next;
+	while (temp != NULL) 
+	{
+		next = temp->next;
+		if (temp->info < 0) 
+		{
+			if (temp == *head) 
+			{
+				*head = temp->next;
+			}
+			if (temp->next != NULL) 
+			{
+				temp->next->prev = temp->prev;
+			}
+			if (temp->prev != NULL) 
+			{
+				temp->prev->next = temp->next;
+			}
+			delete temp;
+		}
+		temp = next;
+	}
 
 }
